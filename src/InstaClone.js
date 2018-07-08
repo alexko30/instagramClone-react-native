27c29465fc1c644173caa7ffa629532065df3727
alexko <rank2505@gmail.com>
2018-07-08 05:01:08 +0300

    70 % finished feed page

--- src/InstaClone.js
+++ src/InstaClone.js
@@ -1,26 +1,116 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Dimensions,
+  TouchableWithoutFeedback,
+  Animated
+} from 'react-native';
+import DoubleClick from 'react-native-double-click';
+// import emoji from 'node-emoji';
+
+import config from './config/index';
 
 class InstaClone extends React.Component {
-
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      screenWidth: 0
+      liked: false,
+      screenWidth: 0,
+      amountLikes: 0,
+      userName: 'Alex30',
+      comment: 'Life is realy beautiful!'
     };
+
+    this.handleDoubleClick = this.handleDoubleClick.bind(this);
+    this.handleOnceClick = this.handleOnceClick.bind(this);
+    this.handleLikePressIn = this.handleLikePressIn.bind(this);
+    this.handleLikePressOut = this.handleLikePressOut.bind(this);
   }
 
   componentDidMount() {
     this.setState({
       screenWidth: Dimensions.get('window').width
     });
+
+    // this.animatedValue = new Animated.Value(1);
+  }
+
+  componentWillMount() {
+    this.animatedValue = new Animated.Value(1);
+  }
+
+  handleDoubleClick() {
+    this.setState({
+      liked: true,
+      amountLikes: this.state.amountLikes + 1
+    });
+  }
+
+  handleLikePressIn() {
+    Animated.spring(this.animatedValue, {
+      toValue: .8
+    }).start()
+  }
+
+  handleLikePressOut() {
+    Animated.spring(this.animatedValue, {
+      toValue: 1,
+      fraction: 2,
+      tension: 100
+    }).start()
+  }
+
+  handleOnceClick() {
+    if (this.state.liked){
+      this.setState({
+        amountLikes: this.state.amountLikes - 1,
+      });
+
+      this.setState({
+        liked: !this.state.liked
+      });
+    }
+
+    else {
+      this.setState({
+        amountLikes: this.state.amountLikes + 1,
+      });
+
+      this.setState({
+        liked: !this.state.liked
+      });
+    }
+
+
   }
 
   render() {
+
+    let amountLikes = String(this.state.amountLikes).split('')
+
+    for (let i = amountLikes.length; i > 0 ; i-=3) {
+      if (i == amountLikes.length) continue;
+      amountLikes.splice(i, 0, ',')
+    }
+
+    amountLikes.join('');
+
+
+    const animatedStyle = {
+      transform: [{ scale: this.animatedValue }]
+    }
+
+    const iconLike = this.state.liked
+      ? config.images.redLikeIcon
+      : config.images.emptyLikeIcon;
+
     return (
       <View style={{ flex: 1, width: 100 + '%', height: 100 + '%' }}>
         <View style={styles.tempNav}>
-          <Text>Intagram</Text>
+          <Image style={{height: 31, width: 110, marginTop: 10}} source={config.images.instagramLogo}/>
         </View>
 
         <View style={styles.userBar}>
@@ -36,17 +126,48 @@ class InstaClone extends React.Component {
           </View>
 
           <View style={{ alignItems: 'center' }}>
-            <Text style={{ fontSize: 26 }}>...</Text>
+            <Text style={{ fontSize: 27 }}>...</Text>
           </View>
         </View>
 
-        <Image
-          style={{ width: this.state.screenWidth, height: this.state.screenWidth * 1.1 }}
-          source={{
-            uri:
-              'https://res.cloudinary.com/dbgewsyxf/image/upload/v1530914350/IMG_0718-1080x1350.jpg'
-          }}
-        />
+        <DoubleClick onClick={this.handleDoubleClick}>
+          <Image
+            style={{
+              width: this.state.screenWidth,
+              height: this.state.screenWidth * 1.2
+            }}
+            source={{
+              uri:
+                'https://res.cloudinary.com/dbgewsyxf/image/upload/v1530914350/IMG_0718-1080x1350.jpg'
+            }}
+          />
+        </DoubleClick>
+
+        <View style={styles.downBar}>
+          <TouchableWithoutFeedback
+            onPress={this.handleOnceClick}
+            onPressIn={this.handleLikePressIn}
+            onPressOut={this.handleLikePressOut}
+          >
+            <Animated.Image
+              style={[styles.icon, { width: 23, height: 23 }, animatedStyle]}
+              source={iconLike}
+            />
+          </TouchableWithoutFeedback>
+          <Image
+            style={[styles.icon, { width: 23, height: 23 }]}
+            source={config.images.commentIcon}
+          />
+          <Image
+            style={[styles.icon, { width: 26, height: 26 }]}
+            source={config.images.messageIcon}
+          />
+        </View>
+
+        <View style={[styles.downBar, styles.downBarLikes]}>
+          <Text style={{paddingLeft: 20}}>{amountLikes} likes</Text>
+          <Text style={{paddingLeft: 20}}>{this.state.userName}   <Text style={styles.commentValue}>{this.state.comment}</Text></Text>
+        </View>
       </View>
     );
   }
@@ -66,7 +187,7 @@ const styles = StyleSheet.create({
 
   userBar: {
     width: 100 + '%',
-    height: 50,
+    height: config.styleConstants.rowHeight,
     backgroundColor: 'white',
     flexDirection: 'row',
     paddingHorizontal: 10,
@@ -77,6 +198,28 @@ const styles = StyleSheet.create({
     width: 40,
     height: 40,
     borderRadius: 20
+  },
+
+  downBar: {
+    height: config.styleConstants.rowHeight,
+    width: 100 + '%',
+    borderColor: 'rgb(234,234,234)',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+
+  downBarLikes: {
+    alignItems: 'flex-start',
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+
+  commentValue: {
+    fontWeight: '100'
+  },
+
+  icon: {
+    marginLeft: 18
   }
 });
 
---
+++ src/config/index.js
@@ -0,0 +1,12 @@
+export default {
+  images: {
+    emptyLikeIcon: require('../../assets/emptyLike.png'),
+    redLikeIcon: require('../../assets/redLike.png'),
+    commentIcon: require('../../assets/comment.png'),
+    messageIcon: require('../../assets/message.png'),
+    instagramLogo: require('../../assets/Instagram_logo.png')
+  },
+  styleConstants: {
+    rowHeight: 50
+  }
+}
